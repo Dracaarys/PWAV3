84f9a0d06b451116c3da5f618575220dcc5cf820
Emilly <emillylimaxx@gmail.com>
2024-08-22 19:59:27 -0300

    Alteração cross

--- ufrn/src/main/java/av3/pw/ufrn/controller/AuthController.java
+++ ufrn/src/main/java/av3/pw/ufrn/controller/AuthController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
     private final ClienteRepository repository;
     private final PasswordEncoder passwordEncoder;
