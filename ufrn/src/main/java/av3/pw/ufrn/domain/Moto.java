3cfa9b96fc16cc519d9377c53e2f6e42914d7140
Emilly <emillylimaxx@gmail.com>
2024-08-23 19:34:36 -0300

    Alterações finais 2

--- ufrn/src/main/java/av3/pw/ufrn/domain/Moto.java
+++ ufrn/src/main/java/av3/pw/ufrn/domain/Moto.java
@@ -15,6 +15,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@SuppressWarnings("unused")
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
